f6f99575100982a288d7d48f8150a959b442ba86
Wilmer <julieannewilmer@gmail.com>
2018-11-07 14:33:49 -0600

    working buttons

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// Dependencies
+var mysql = require("mysql");
+require('dotenv').config();
+var mysql_pass = process.env.MYSQL_PASS;
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: mysql_pass,
+  database: "shake_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
